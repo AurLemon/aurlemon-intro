0c7a55d1694c02edcbfc0f3492b179e58eec0943
AurLemon <aurlemoncn@gmail.com>
2025-02-11 07:15:06 +0800

    Daily commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,13 +3,16 @@ import { PrismaClient } from '@prisma/client'
 const config: any = useRuntimeConfig()
 
 const prismaClientSingleton = () => {
+    // 神医：
+    // https://stackoverflow.com/questions/78550989/tables-do-not-exist-in-prisma-database-after-nuxt-app-deployment
+    // https://github.com/nuxt/nuxt/issues/21753
     return new PrismaClient({
         datasources: {
             db: {
                 url: config.bdUrl
             }
         }
-        })
+    })
 }
 
 declare const globalThis: {
